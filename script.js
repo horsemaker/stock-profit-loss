d6e237cfa5d625e8877fd22bc5e32c28530a5305
horsemaker <yash_ghodekar@yahoo.in>
2021-08-05 21:05:21 +0530

    feat: theme changing

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+var root = document.documentElement
+var profitEmojis = document.querySelector('#profitEmojis')
+var lossEmojis = document.querySelector('#lossEmojis')
+var heroImg = document.querySelector('#hero-img')
 var currentPriceInput = document.querySelector('#current-price')
 var purchasePriceInput = document.querySelector('#purchase-price')
 var stockQuantityInput = document.querySelector('#stock-quantity')
 var checkAction = document.querySelector('#check')
+var result = document.querySelector('#result')
 var resultText = document.querySelector('#result-text')
 
 function giveResult(event) {
@@ -20,8 +25,25 @@ function giveResult(event) {
       (currentPrice - purchasePrice) *
       stockQuantity
     ).toFixed(2)
-    console.log('Percent Profit:', percentProfit)
-    console.log('Absolute Profit:', absoluteProfit)
+    // console.log('Percent Profit:', percentProfit)
+    // console.log('Absolute Profit:', absoluteProfit)
+
+    if (percentProfit >= 50) {
+      root.style.setProperty('--primary-color', '#8AFF8A')
+      root.style.setProperty('--secondary-color', '#065F46')
+      root.style.setProperty('--tertiary-color', 'white')
+      root.style.setProperty('--quaternary-color', '#064E3B')
+      heroImg.src = 'assets/profit.svg'
+    }
+
+    lossEmojis.style.display = 'none'
+    profitEmojis.style.display = 'block'
+    result.style.display = 'block'
+
+    setTimeout(function () {
+      profitEmojis.style.display = 'none'
+    }, 30000)
+
     resultText.textContent =
       'You gained ' +
       percentProfit +
@@ -36,8 +58,25 @@ function giveResult(event) {
     var absoluteLoss = ((purchasePrice - currentPrice) * stockQuantity).toFixed(
       2
     )
-    console.log('Percent Loss:', percentLoss)
-    console.log('Absolute Loss:', absoluteLoss)
+    // console.log('Percent Loss:', percentLoss)
+    // console.log('Absolute Loss:', absoluteLoss)
+
+    if (percentLoss >= 50) {
+      root.style.setProperty('--primary-color', '#EF4444')
+      root.style.setProperty('--secondary-color', '#991B1B')
+      root.style.setProperty('--tertiary-color', 'white')
+      root.style.setProperty('--quaternary-color', '#7F1D1D')
+      heroImg.src = 'assets/loss.svg'
+    }
+
+    profitEmojis.style.display = 'none'
+    lossEmojis.style.display = 'block'
+    result.style.display = 'block'
+
+    setTimeout(function () {
+      lossEmojis.style.display = 'none'
+    }, 30000)
+
     resultText.textContent =
       'You lost ' + percentLoss + '%. Your total loss is ' + absoluteLoss + '.'
   } else if (currentPrice === purchasePrice) {
